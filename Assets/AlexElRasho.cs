b0ebfed5072b38c091ca12a64777c5e307289084
alexqv2 <alex-92quintana@hotmail.com>
2020-05-18 17:57:51 -0500

    .

--- Assets/AlexElRasho.cs
+++ Assets/AlexElRasho.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class AlexElRasho : MonoBehaviour
@@ -7,13 +8,13 @@ public class AlexElRasho : MonoBehaviour
     AlexSonido alexSonido;
     AlexScript alexBolitas;
     Renderer alexCubito;
-    int t = 0;
-    int c = 0;
     float sy = 1;
     float sx = 1;
     float sz = 1;
-    float lineG = 0.125f;
-    float large = 80;
+    float lineG = 0.25f;
+    float large = 1024;
+    int side;
+    int c = 27;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,26 +26,48 @@ public class AlexElRasho : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (alexBolitas.lightFrecuency < 514)
-        {
-            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(large / sx, lineG / sy, lineG / sz));
-            sx = large;
-            sz = lineG;
-            sy = lineG;
+        c++;
+        if (c == 28){
+            UnityEngine.Debug.Log(transform.localScale.x + " - "+transform.localScale.y + " - "+ transform.localScale.z);
+            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1 / sx, 1 / sy, 1 / sz));
+            if (alexBolitas.lightFrecuency < 514)
+            {
+                side = 1;
+                sx = large;
+                sy = 1;
+                sz = 1;
+            }
+            else if (alexBolitas.lightFrecuency < 648)
+            {
+                side = 2;
+                sx = 1;
+                sy = large;
+                sz = 1;
+            }
+            else
+            {
+                side = 3;
+                sx = 1;
+                sy = 1;
+                sz = large;
+            }
+            c = 0;
         }
-        else if (alexBolitas.lightFrecuency < 648)
-        {
-            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(lineG / sx, large / sy, lineG / sz));
-            sx = lineG;
-            sz = lineG;
-            sy = large;
-        }
-        else
-        {
-            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(lineG / sx, lineG / sy, large / sz));
-            sx = lineG;
-            sz = large;
-            sy = lineG;
+        if (c % 3 == 0){
+            switch (side)
+            {
+                case 1:
+                    transform.localScale = Vector3.Scale(transform.localScale, new Vector3(2, 1, 1));
+                    break;
+                case 2:
+                    transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1, 2, 1));
+                    break;
+                case 3:
+                    transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1, 1, 2));
+                    break;
+                default:
+                    break;
+            }
         }
         transform.Rotate(0.5f, 0.5f, 0.5f);
         alexCubito.material.color = new Color(1 - alexBolitas.r, 1 - alexBolitas.g, 1 - alexBolitas.b);
